4209a6c9b7344632bf2dfdb7d188c7a42f7c38b4
Alexis_Lienard <lienard.alexis@gmail.com>
2023-12-20 13:31:04 +0100

    + added rootDir && outDir path + types basics

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+// arrays
+let names = ["Alexis", "Antoine", "Albane"];
+let ages = [25, 28, 24];
+names.push("Dider");
+ages.push(50);
+// type inference with arrays
+let fruits = ["apples", " pears", "bananas", "mangos"];
+fruits.push("peaches");
+const f = fruits[3];
+let things = [1, true, "hello"];
+const t = things[0];
+// object literals
+let user = {
+    firstname: "Alexis",
+    age: 30,
+    id: 1,
+};
+user.firstname = "Antoine";
+user.id = 2;
+// type inference with object literals
+let person = {
+    name: "luigi",
+    score: 30,
+};
+person.name = "mario";
+person.score = 40;
+const score = person.score;
+console.log(score);
