55c652227c27edb4192fedd4466db27e34b253f6
Alexis_Lienard <lienard.alexis@gmail.com>
2023-12-20 13:18:01 +0100

    type basics

---
+++ index.js
@@ -0,0 +1,14 @@
+var age = 30;
+var firstName = "Alex";
+var isReal;
+firstName = "Antoine";
+isReal = true;
+var planet = "Earth";
+var moons = 1;
+var isLarge = false;
+planet = "Saturn";
+moons = 145;
+isLarge = true;
+// null & undefined
+var something;
+var anotherThing;
